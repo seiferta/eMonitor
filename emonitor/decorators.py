ed7d9852ec7079d95951100ca24faa72aff2831c
Arne Seifert <arne.seifert@tum.de>
2014-08-05 11:41:06 +0200

    new decorator admin_required added

--- emonitor/decorators.py
+++ emonitor/decorators.py
@@ -11,3 +11,12 @@ def login_required(f):
             return login_manager.unauthorized()
         return f(*args, **kwargs)
     return decorated_function
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if (not current_user.is_authenticated()) or current_user.level != 1:
+            return login_manager.unauthorized()
+        return f(*args, **kwargs)
+    return decorated_function
