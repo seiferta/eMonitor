3179139d1e74db0977d3459999f63bbd8f0cf0e3
Arne Seifert <arne@seifert-online.net>
2014-12-16 15:20:09 +0100

    docstring for blueprint admin added

--- emonitor/admin/admin.py
+++ emonitor/admin/admin.py
@@ -1,3 +1,14 @@
+"""
+Basic framework for admin area (blueprint).
+Modules can register an admin area that will be loaded automaticaly into the area with an own menu entry.
+
+Use the info parameter of the module implementation:
+::
+
+    info = dict(area=['admin'], name='modulename', path='modulepath', ...)
+
+This example will add the module 'modulename' in admin area with url `server/admin/modulepath`
+"""
 from collections import OrderedDict
 import flask.ext.login as login
 from emonitor.sockethandler import SocketHandler
@@ -13,6 +24,11 @@ admin.processes = []  # dict for admin processes
 
 
 def init_app(app):
+    """
+    Do all init operations needed for this blueprint
+
+    :param app: :py:class:`Flask` app
+    """
     signal.addSignal('admin', 'processes')
     signal.connect('admin', 'processes', adminHandler.handleProcesses)
 
@@ -20,6 +36,14 @@ admin.init_app = init_app
 
 
 def addModule(module):
+    """
+    Add module for admin area
+
+    External modules can have a section in the admin area.
+    In the init process of the module *addModule()* will be executed to register the module for the admin area
+
+    :param module: :py:class:`emonitor.utils.Module` object
+    """
     if module.info['name'] not in admin.modules.keys():
         admin.modules[module.info['name']] = module
 
@@ -27,6 +51,11 @@ admin.addModule = addModule
 
 
 def addProcess(process):
+    """
+    The admin area uses processes. External modules can add own *processes* needed for background operations
+
+    :param process: process object
+    """
     if process not in admin.processes:
         admin.processes.append(process)
         #signal.send('admin.processes')
@@ -44,6 +73,15 @@ babel.gettext(u'trigger.file_removed')
 @admin.route('/admin/<path:module>', methods=['GET', 'POST'])
 @admin_required
 def adminContent(module=''):
+    """
+    Admin area is reachable under *http://[servername]:[port]/admin/[module]*
+
+    *module* will execute the *getAdminContent* method of registered module. If no nodule is given, the startpage of
+    the admin area will be used
+
+    :param module: module name as string
+    :return: rendered HTML-output of module or startpage
+    """
     if module == 'upgradedb':  # create new db-revision name given by revisionname
         if request.args.get('revisionname', '') != '':
             msg = alembic.revision(request.args.get('revisionname'))
@@ -58,6 +96,15 @@ def adminContent(module=''):
 @admin.route('/admin/data/', methods=['GET', 'POST'])
 @admin.route('/admin/data/<path:module>', methods=['GET', 'POST'])
 def adminData(module=''):
+    """
+    Admin area is reachable unter *http://[servername]:[port]/admin/data/[module]*
+
+    This path is used to run background operations (e.g. ajax calls) and delivers the result of the `getAdminData`
+    method of the module. If **format=json** in the url the result will be formated as json
+
+    :param module: module name as string
+    :return: return value of `getAdminData` method of `module`
+    """
     current_mod = [admin.modules[m] for m in admin.modules if admin.modules[m].info['path'] == module.split('/')[0]][0]
     result = current_mod.getAdminData()
     if request.args.get('format') == 'json':  # reformat to json
@@ -69,26 +116,44 @@ def adminData(module=''):
 # static folders
 @admin.route('/admin/img/<path:filename>')
 def imagebase_static(filename):
+    """
+    Register url path *http://[servername]:[port]/admin/img/[filename]* for static files
+
+    :param filename: filename as string
+    """
     return send_from_directory(admin.root_path + '/web/img/', filename)
 
 
 @admin.route('/admin/js/<path:filename>')
 def jsbase_static(filename):
+    """
+    Register url path *http://[servername]:[port]/admin/js/[filename]* for static files
+
+    :param filename: filename as string
+    """
     return send_from_directory(admin.root_path + '/web/js/', filename)
 
 
 @admin.route('/admin/css/<path:filename>')
 def cssbase_static(filename):
+    """
+    Register url path *http://[servername]:[port]/admin/css/[filename]* for static files
+
+    :param filename: filename as string
+    """
     return send_from_directory(admin.root_path + '/web/css/', filename)
 
 
 class adminHandler(SocketHandler):
     @staticmethod
     def handleProcesses(sender, **extra):
-        # TODO: change process info
-        print "EXTRAS;", extra
+        """
+        Handler for background processes
 
-        if 'id' in extra:
+        :param sender: operation class or module
+        :param extra: parameters of process as dict list
+        """
+        if 'id' in extra:  # TODO CHANGE PROCESS INFO
             SocketHandler.send_message(str(extra['id']))
         else:
             SocketHandler.send_message('admin.processes', **extra)
