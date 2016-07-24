fc0016d3bc4fa231ba99dd8451b7f80d723b720f
seiferta <arne@seifert-online.net>
2016-07-24 10:28:04 +0200

    new event for filename fix added to correct filename if bad chars in name

--- emonitor/modules/textmod/__init__.py
+++ emonitor/modules/textmod/__init__.py
@@ -1,3 +1,7 @@
+import os
+import datetime
+import time
+import shutil
 from emonitor.utils import Module
 from emonitor.extensions import events, babel
 from .content_admin import getAdminContent, getAdminData
@@ -25,6 +29,7 @@ class TextmodModule(Module):
         from .ocr import Ocr
 
         # eventhandlers
+        events.addHandlerClass('file_added', 'emonitor.modules.textmod.TextmodModule', TextmodModule.handleEvent, ['in.path', 'in.filename', 'out.filename'])
         events.addHandlerClass('file_added', 'emonitor.modules.textmod.ocr.Ocr', Ocr.handleEvent, ['in.path', 'in.filename', 'out.text'])
         events.addHandlerClass('file_added', 'emonitor.modules.textmod.replace.Replace', Replace.handleEvent, ['in.text', 'out.text'])
         
@@ -36,6 +41,32 @@ class TextmodModule(Module):
         babel.gettext(u'module.textmod.convert')
         babel.gettext(u'emonitor.modules.textmod.ocr.Ocr')
         babel.gettext(u'emonitor.modules.textmod.replace.Replace')
+        babel.gettext(u'emonitor.modules.textmod.TextmodModule')
+
+    @staticmethod
+    def handleEvent(eventname, **kwargs):
+        """
+        replace income filename with date as name to prevent exception in handler operation and move file in filesystem
+
+        :param eventname: unused
+        :param kwargs: all given parameters of current event chain
+        :return: changed filename parameter, others unchanged
+        """
+        if 'time' not in kwargs:
+            kwargs['time'] = []
+        if 'incomepath' not in kwargs or 'filename' not in kwargs:
+            kwargs['time'].append(u'textmod: missing parameters for textmod, nothing done.')
+            return kwargs
+
+        kwargs['time'].append(u'textmod: fix filenames.')
+        stime = time.time()
+
+        filename = "{}{}".format('{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now()), os.path.splitext(kwargs.get('filename'))[-1])
+        shutil.move('{}{}'.format(kwargs.get('incomepath'), kwargs.get('filename')), '{}{}'.format(kwargs.get('incomepath'), filename))
+        kwargs['filename'] = filename
+        t = time.time() - stime
+        kwargs['time'].append(u'textmod: filename changed to "{}" done in {} sec.'.format(filename, t))
+        return kwargs
 
     def getAdminContent(self, **params):
         """
