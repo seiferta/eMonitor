8b2ed23b755e6b6020e6aa2fb96448b71a277ef4
Tobias Bystricky <tobias@bystricky.de>
2016-09-09 22:29:51 +0200

    #48 Alarmeingang über serielle Konsole: Add parsing of incoming serial stream

--- emonitor/serialobserver.py
+++ emonitor/serialobserver.py
@@ -1,27 +1,61 @@
 import logging
 import serial
-from threading import Thread
+import re
+import datetime
 from emonitor.extensions import events
 from .extensions import scheduler
 
 logger = logging.getLogger(__name__)
 connected = False
-events.addEvent('incoming_serial_data', handlers=[], parameters=['message'])
+events.addEvent('incoming_serial_data', handlers=[], parameters=['out.message'])
 
 
 def handleIncomingData(data):
-    events.raiseEvent('incoming_serial_data', message=data)
-    logger.info(u"incoming_serial_data: {}{}".format(data))
+    alarm_fields = dict()
+
+    alarmmessage = re.search(b'\x02([\s\S]*)\x03', data, re.MULTILINE)
+    if alarmmessage:
+        logger.info(u"Received valid alarmmessage from serial: {}".format(alarmmessage.groups()))
+        strippedMessage = alarmmessage.group(1).strip()
+        stringParts = strippedMessage.split(":")
+        if len(stringParts) == 2:
+            logger.info(u"Key: {}".format(stringParts[0]))
+            alarm_fields['key'] = stringParts[0]
+            addressParts = stringParts[1].split(",")
+            if len(addressParts) == 5:
+                alarm_fields['city'] = addressParts[0]
+                alarm_fields['address'] = addressParts[1]
+                alarm_fields['address2'] = addressParts[2]
+                alarm_fields['remark'] = addressParts[3]
+
+            else:
+                logger.error("Invalid message received")
+        else:
+            logger.error("Invalid message received")
+    else:
+        logger.info("Received invalid alarm message from serial")
+
+    uhrzeit = re.search(b'\x03([\s\S]*)\\r\\n', data, re.MULTILINE | re.DOTALL)
+    if uhrzeit:
+        logger.info(u"Received valid datetime from serial {}".format(uhrzeit.group(1).strip()))
+        try:
+            t = datetime.datetime.strptime(uhrzeit.group(1).strip(), '%d.%m.%Y %H:%M:%S')
+        except ValueError:
+            t = datetime.datetime.now()
+        alarm_fields['time'] = t
+
+    events.raiseEvent('incoming_serial_data', message=alarm_fields)
 
 
 def readFromPort(ser):
     global connected
     while not connected:
         connected = True
-
-        while True:
-           reading = ser.readline().decode()
-           handleIncomingData(reading)
+        while connected:
+            firstData = ser.read(1);
+            bytesToRead = ser.inWaiting()
+            if bytesToRead > 0:
+                handleIncomingData(firstData + ser.read(bytesToRead))
 
     ser.close()
 
